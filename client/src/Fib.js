0789e993aa3d0742693cfa82e3a26c0c4e269a87
Hiep Tran <hieptt149@gmail.com>
2025-08-17 15:59:17 +0700

    Add client folder again

---
+++ client/src/Fib.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect, use } from "react";
+import axios from "axios";
+
+function Fib() {
+  const [seenIndexes, setSeenIndexes] = useState([]);
+  const [values, setValues] = useState({});
+  const [index, setIndex] = useState("");
+
+  useEffect(() => {
+    fetchValues();
+    fetchIndexes();
+  }, []);
+
+  async function fetchValues() {
+    const values = await axios.get("/api/values/current");
+    setValues(values.data);
+  }
+
+  async function fetchIndexes() {
+    const seenIndexes = await axios.get("/api/values/all");
+    setSeenIndexes(seenIndexes.data);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    await axios.post("/api/values", {
+      index: index,
+    });
+
+    setIndex("");
+  }
+
+  function renderSeenIndexes() {
+    return seenIndexes.map(({ number }) => number).join(", ");
+  }
+
+  function renderValues() {
+    const entries = [];
+    for (let key in values) {
+      entries.push(
+        <div key={key}>
+          For index {key} I calculated {values[key]}
+        </div>
+      );
+    }
+    return entries;
+  }
+
+  return (
+    <div>
+      <h1>Fibonacci Calculator</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Enter your index:</label>
+        <input value={index} onChange={(e) => setIndex(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+      <h3>Indexes I have seen:</h3>
+      {renderSeenIndexes()}
+      <h3>Calculated Values:</h3>
+      {renderValues()}
+    </div>
+  );
+}
+
+export default Fib;
---
+++ client/src/OtherPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function OtherPage() {
+  return (
+    <div>
+      <h1>Other Page</h1>
+      <p>This is another page in the application.</p>
+      <Link to="/">Go back to Home</Link>
+    </div>
+  );
+}
+
+export default OtherPage;
